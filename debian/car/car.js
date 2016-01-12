938d23ef95506422c69e32f1a2c7ec3ff7ce3809
Raphael Beer <rbeerdev@gmail.com>
2016-01-12 07:30:39 +0100

    Change probe cb

--- debian/car/car.js
+++ debian/car/car.js
@@ -9,10 +9,6 @@ var rl = require('readline').createInterface({
 });
 
 
-var vncup = false;
-testVNC(function(err, port) {
-  console.log(port);
-});
 rl.question('Press key to start...', function(evt) {
 
   var c = net.connect({
@@ -31,28 +27,17 @@ rl.question('Press key to start...', function(evt) {
     }
     if (cmd === 'reqvnc') {
       var vncto = setInterval(function() {
-        testVNC(function(err, avail) {
+        testVNC(function(err, port) {
           if (err) {
-            vncup = false;
-
-            if (err.code === 3) {
-              // not running ("failed")
-              avail = false;
-            } else if (err.code === 127) {
-              // service unit not found
-              clearInterval(vncto);
-              return console.log('Service unit onlinux-vnc is not installed.');
+            c.write('wait');
+            if (err.toString() === 'EVNCDOWN') {
+              return console.log('VNC not up yet...');
             } else {
               return console.log(err);
             }
           }
-          vncup = avail;
-          if (vncup) {
-            c.write('goahead');
-            clearInterval(vncto);
-          } else {
-            c.write('wait');
-          }
+          c.write('goahead');
+          clearInterval(vncto);
         });
       }, 1000);
     }
@@ -61,23 +46,21 @@ rl.question('Press key to start...', function(evt) {
     console.log('disconnected from server');
   });
 
-});
   function testVNC(cb) {
     console.log('Probing for VNC:');
     var cmd = 'netstat -npl | sed -nr \'s/.*?[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+:([0-9]+).*?x11vnc/\\1/ p\'';
     exec(cmd, function(err, stdout, stderr) {
       var port;
       if (err) {
-        console.log('failed.');
         return cb(err);
       }
       port = parseInt(stdout.toString()) || 0;
       if (port !== 0) {
-        console.log('running.');
         return cb(null, port);
       } else {
-        console.log('VNC is not running');
-        return cb(null, false);
+        return cb(new Error('EVNCDOWN'));
       }
     });
   };
+
+});
